6b3bc369b096868fd5dfeb2ade6d52a3521edd70
Matheus Vilela Antunes de Oliveira <64479369+Matheus-mVilela@users.noreply.github.com>
2021-11-29 13:01:06 -0300

    finish api (#8)

--- todolist/app/urls.py
+++ todolist/app/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
-from rest_framework import routers
 from app import views
 
 
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
-    path("listsoftasks/", views.ListTaskViewSet.as_view()),
-    path("listsoftasks/<int:list_task_pk>", views.ListTaskViewSet.as_view()),
+    path("listsoftasks/", views.ListOfTaskViewSet.as_view()),
+    path("listsoftasks/<int:list_task_pk>", views.ListOfTaskViewSet.as_view()),
     path("tasks/", views.TaskViewSet.as_view()),
     path("tasks/<int:task_pk>", views.TaskViewSet.as_view()),
 ]
